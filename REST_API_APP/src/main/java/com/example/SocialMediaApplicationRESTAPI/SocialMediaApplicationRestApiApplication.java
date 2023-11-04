6e3b616cacb6ed0086bf5fc387e2ac2dddd5fd07
krutik2377 <krutikgevariya7723@gmail.com>
2023-11-04 14:43:14 -0400

    initial Commit

---
+++ REST_API_APP/src/main/java/com/example/SocialMediaApplicationRESTAPI/SocialMediaApplicationRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocialMediaApplicationRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApplicationRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApplicationRestApiApplication.class, args);
+	}
+
+}
---
+++ REST_API_APP/src/test/java/com/example/SocialMediaApplicationRESTAPI/SocialMediaApplicationRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialMediaApplicationRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
