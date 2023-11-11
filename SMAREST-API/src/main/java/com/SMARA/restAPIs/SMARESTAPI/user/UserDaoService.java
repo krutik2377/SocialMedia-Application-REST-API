fea844bb3e4e883e162229ddd1f458389cce5f81
krutik2377 <krutikgevariya7723@gmail.com>
2023-11-11 17:59:49 -0500

    Implemented Post Method to create a User Resource

--- SMAREST-API/src/main/java/com/SMARA/restAPIs/SMARESTAPI/user/UserDaoService.java
+++ SMAREST-API/src/main/java/com/SMARA/restAPIs/SMARESTAPI/user/UserDaoService.java
@@ -12,27 +12,35 @@ public class UserDaoService {
 	//JPA/Hibernate -> Database
 	// UserDaoService - > Static List
 	
+	private static int usersCount = 0;
+	
 	private static List<User> users = new ArrayList<>();
 	
 	static {
-		users.add(new User(1,"Krutik", LocalDate.now().minusYears(30)));
-		users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
-		users.add(new User(3,"Jim", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount ,"Krutik", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount,"Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount,"Jim", LocalDate.now().minusYears(20)));
 	}
 	
 	//public List<User> findAll()
 	public List<User> findAll() {
 		return users;
+		
 	}
 	
+	public User save(User user)
+	{
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
 	
 	//public User findOne(int id)
 	public User findUserbyId(int userId)
 	{
 		// Also can return but looping and finding
 		Predicate<? super User> predicate  = user -> user.getId().equals(userId);
-		return users.stream().filter(predicate).findFirst().get();
-			
+		return users.stream().filter(predicate).findFirst().get();	
 	}
 	
 	
