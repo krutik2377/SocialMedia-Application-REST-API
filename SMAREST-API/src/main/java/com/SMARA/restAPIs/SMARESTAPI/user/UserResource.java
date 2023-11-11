0700d9152ec72010113992ec862f5dc2aff390f9
krutik2377 <krutikgevariya7723@gmail.com>
2023-11-11 18:19:53 -0500

    Enhanced POST method to return correct HTTP status code and URI (Location)

--- SMAREST-API/src/main/java/com/SMARA/restAPIs/SMARESTAPI/user/UserResource.java
+++ SMAREST-API/src/main/java/com/SMARA/restAPIs/SMARESTAPI/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.SMARA.restAPIs.SMARESTAPI.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -32,8 +35,15 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void creatUser(@RequestBody User user)
+	public ResponseEntity<Object> creatUser(@RequestBody User user)
 	{
-		service.save(user);
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		// Location header :- URL location /users/4
+		return ResponseEntity.created(location ).build();
 	}
 }
